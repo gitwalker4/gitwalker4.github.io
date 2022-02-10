1ec05558755f105a0b606d793078716bf281db56
gitwalker4 <tolbertwalker4@gmail.com>
2022-02-10 12:27:58 -0800

    added slide mechanism

--- js/app.js
+++ js/app.js
@@ -12,49 +12,53 @@ const gameShow = document.querySelector('.gameShow');
 const gallery = document.querySelector('.gallery');
 const webDash = document.querySelector('.webDash');
 const employeeDir = document.querySelector('.empDir');
-const prev = document.querySelector('.prev');
-const next = document.querySelector('.next');
+const dot1 = document.querySelector('.dot-1');
+const dot2 = document.querySelector('.dot-2');
+const dot3 = document.querySelector('.dot-3');
+const dot4 = document.querySelector('.dot-4');
 
-prev.addEventListener('click', () => {
-  if ( gameShow ) {
-    gameShow.classList.replace('col-lg-8', 'd-lg-none');
-    employeeDir.classList.replace('d-lg-none', 'col-lg-8');
-  }
-  
-  else if ( gallery ) {
-    gallery.classList.replace('col-lg-8', 'd-lg-none');
-    gameShow.classList.replace('d-lg-none', 'col-lg-8');
-  } 
-  
-  else if ( webDash ) {
-    webDash.classList.replace('col-lg-8', 'd-lg-none');
-    gallery.classList.replace('d-lg-none', 'col-lg-8');
-  }
-  
-  else if ( employeeDir ) {
-    employeeDir.classList.replace('col-lg-8', 'd-lg-none');
-    webDash.classList.replace('d-lg-none', 'col-lg-8');
-  }
+dot1.addEventListener('click', () => {
+  dot1.classList.add('active');
+  dot2.classList.remove('active');
+  dot3.classList.remove('active');
+  dot4.classList.remove('active');
+  gallery.classList.replace('col-lg-8', 'd-lg-none');
+  webDash.classList.replace('col-lg-8', 'd-lg-none');
+  employeeDir.classList.replace('col-lg-8', 'd-lg-none');
+  gameShow.classList.replace('d-lg-none', 'col-lg-8');
 });
 
-next.addEventListener('click', () => {
-  if ( gameShow ) {
-    gameShow.classList.replace('col-lg-8', 'd-lg-none');
-    gallery.classList.replace('d-lg-none', 'col-lg-8');
-  } 
-  
-  else if ( gallery ) {
-    gallery.classList.replace('col-lg-8', 'd-lg-none');
-    webDash.classList.replace('d-lg-none', 'col-lg-8');
-  } 
-  
-  else if ( webDash ) {
-    webDash.classList.replace('col-lg-8', 'd-lg-none');
-    employeeDir.classList.replace('d-lg-none', 'col-lg-8');
-  } 
-  
-  else if ( employeeDir ) {
-    employeeDir.classList.replace('col-lg-8', 'd-lg-none');
-    gameShow.classList.replace('d-lg-none', 'col-lg-8');
-  }
+dot2.addEventListener('click', () => {
+  dot2.classList.add('active');
+  dot1.classList.remove('active');
+  dot3.classList.remove('active');
+  dot4.classList.remove('active');
+  gameShow.classList.replace('col-lg-8', 'd-lg-none');
+  webDash.classList.replace('col-lg-8', 'd-lg-none');
+  employeeDir.classList.replace('col-lg-8', 'd-lg-none');
+  gallery.classList.replace('d-lg-none', 'col-lg-8');
 });
+
+dot3.addEventListener('click', () => {
+  dot3.classList.add('active');
+  dot1.classList.remove('active');
+  dot2.classList.remove('active');
+  dot4.classList.remove('active');
+  gameShow.classList.replace('col-lg-8', 'd-lg-none');
+  gallery.classList.replace('col-lg-8', 'd-lg-none');
+  employeeDir.classList.replace('col-lg-8', 'd-lg-none');
+  webDash.classList.replace('d-lg-none', 'col-lg-8');
+});
+
+dot4.addEventListener('click', () => {
+  dot4.classList.add('active');
+  dot1.classList.remove('active');
+  dot2.classList.remove('active');
+  dot3.classList.remove('active');
+  gameShow.classList.replace('col-lg-8', 'd-lg-none');
+  gallery.classList.replace('col-lg-8', 'd-lg-none');
+  webDash.classList.replace('col-lg-8', 'd-lg-none');
+  employeeDir.classList.replace('d-lg-none', 'col-lg-8');
+});
+
+const drapes = document.querySelector('.drapes');
