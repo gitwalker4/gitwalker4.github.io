6345c8d6287cbba86de039946944c4191a0add9e
gitwalker4 <tolbertwalker4@gmail.com>
2022-02-09 16:15:59 -0800

    added JS event listeners

--- js/app.js
+++ js/app.js
@@ -17,60 +17,44 @@ const next = document.querySelector('.next');
 
 prev.addEventListener('click', () => {
   if ( gameShow ) {
-    gameShow.classList.remove('col-lg-8');
-    gameShow.classList.add('d-lg-none');
-    employeeDir.classList.remove('d-lg-none');
-    employeeDir.classList.add('col-lg-8');
+    gameShow.classList.replace('col-lg-8', 'd-lg-none');
+    employeeDir.classList.replace('d-lg-none', 'col-lg-8');
   }
   
   else if ( gallery ) {
-    gallery.classList.remove('col-lg-8');
-    gallery.classList.add('d-lg-none');
-    gameShow.classList.remove('d-lg-none');
-    gameShow.classList.add('col-lg-8');
+    gallery.classList.replace('col-lg-8', 'd-lg-none');
+    gameShow.classList.replace('d-lg-none', 'col-lg-8');
   } 
   
   else if ( webDash ) {
-    webDash.classList.remove('col-lg-8');
-    webDash.classList.add('d-lg-none');
-    gallery.classList.remove('d-lg-none');
-    gallery.classList.add('col-lg-8');
+    webDash.classList.replace('col-lg-8', 'd-lg-none');
+    gallery.classList.replace('d-lg-none', 'col-lg-8');
   }
   
   else if ( employeeDir ) {
-    employeeDir.classList.remove('col-lg-8');
-    employeeDir.classList.add('d-lg-none');
-    webDash.classList.remove('d-lg-none');
-    webDash.classList.add('col-lg-8');
+    employeeDir.classList.replace('col-lg-8', 'd-lg-none');
+    webDash.classList.replace('d-lg-none', 'col-lg-8');
   }
 });
 
 next.addEventListener('click', () => {
   if ( gameShow ) {
-    gameShow.classList.remove('col-lg-8');
-    gameShow.classList.add('d-lg-none');
-    gallery.classList.remove('d-lg-none');
-    gallery.classList.add('col-lg-8');
+    gameShow.classList.replace('col-lg-8', 'd-lg-none');
+    gallery.classList.replace('d-lg-none', 'col-lg-8');
   } 
   
   else if ( gallery ) {
-    gallery.classList.remove('col-lg-8');
-    gallery.classList.add('d-lg-none');
-    webDash.classList.remove('d-lg-none');
-    webDash.classList.add('col-lg-8');
+    gallery.classList.replace('col-lg-8', 'd-lg-none');
+    webDash.classList.replace('d-lg-none', 'col-lg-8');
   } 
   
   else if ( webDash ) {
-    webDash.classList.remove('col-lg-8');
-    webDash.classList.add('d-lg-none');
-    employeeDir.classList.remove('d-lg-none');
-    employeeDir.classList.add('col-lg-8');
+    webDash.classList.replace('col-lg-8', 'd-lg-none');
+    employeeDir.classList.replace('d-lg-none', 'col-lg-8');
   } 
   
   else if ( employeeDir ) {
-    employeeDir.classList.remove('col-lg-8');
-    employeeDir.classList.add('d-lg-none');
-    gameShow.classList.remove('d-lg-none');
-    gameShow.classList.add('col-lg-8');
+    employeeDir.classList.replace('col-lg-8', 'd-lg-none');
+    gameShow.classList.replace('d-lg-none', 'col-lg-8');
   }
 });
